4403b793bd035952ae5a2101d0c7d0ad84104b6d
NMRayhan <nurmdrayhan97@gmail.com>
2022-02-03 17:24:19 +0600

    Object Oriented Programming with PHP

--- app/classes/Example.php
+++ app/classes/Example.php
@@ -1,7 +1,18 @@
 <?php
+
 namespace App\classes;
 
 class Example
 {
+    public $userAge = 25;
+    public $firstName = "Nur Mohammad ";
+    public $lastName = "Rayhan";
 
+
+    public function index()
+    {
+        $firstName = $this->firstName;
+        $lastName = $this->lastName;
+        echo $firstName.$lastName;
+    }
 }
